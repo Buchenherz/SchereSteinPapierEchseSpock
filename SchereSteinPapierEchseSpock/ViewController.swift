7ae557c723ecde1e629e8b4ebae7d1764b74da0c
Clemens Pfister <pfistecl@me.com>
2016-08-04 19:33:49 +0200

    Initial Commit

---
+++ SchereSteinPapierEchseSpock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SchereSteinPapierEchseSpock
+//
+//  Created by Clemens Pfister on 04/08/2016.
+//  Copyright © 2016 Clemens Pfister. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
